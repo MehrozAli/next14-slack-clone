c28ce5657079d8a91fb8586c56506bf7a6af2c2a
Mehroz Ali <dev.mehrozali@gmail.com>
2024-09-29 21:12:01 +0500

    Auth screens completed + initialize convex for database

--- src/features/auth/components/SignInCard.tsx
+++ src/features/auth/components/SignInCard.tsx
@@ -1,5 +1,93 @@
-export const SignInCard = () => {
-  return (
-    <div>SignInCard</div>
-  )
+"use client";
+
+import { Dispatch, SetStateAction, useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { SignInFlow } from "../types";
+
+interface SignInCardProps {
+  setState: Dispatch<SetStateAction<SignInFlow>>;
 }
+
+export const SignInCard = ({ setState }: SignInCardProps) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <Card className="h-full w-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Login to continue</CardTitle>
+
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value={formData.email}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, email: e.target.value }))
+            }
+            placeholder="Email"
+            type="email"
+            required
+          />
+
+          <Input
+            disabled={false}
+            value={formData.password}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, password: e.target.value }))
+            }
+            placeholder="Password"
+            type="password"
+            required
+          />
+
+          <Button size="lg" type="submit" className="w-full">
+            Continue
+          </Button>
+        </form>
+
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button className="relative w-full" size="lg" variant="outline">
+            <FcGoogle className="size-5 absolute top-3 left-2.5" />
+            Continue with Google
+          </Button>
+
+          <Button className="relative w-full" size="lg" variant="outline">
+            <FaGithub className="size-5 absolute top-3 left-2.5" />
+            Continue with GitHub
+          </Button>
+        </div>
+
+        <p className="text-xs text-muted-foreground">
+          Don&apos;t have an account?
+          <span
+            onClick={() => setState("signUp")}
+            className="text-sky-700 hover:underline cursor-pointer"
+          >
+            Sign up
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
--- src/features/auth/components/SignUpCard.tsx
+++ src/features/auth/components/SignUpCard.tsx
@@ -1,5 +1,108 @@
-export const SignUpCard = () => {
-  return (
-    <div>SignUpCard</div>
-  )
+"use client";
+
+import { Dispatch, SetStateAction, useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { SignInFlow } from "../types";
+
+interface SignUpCardProps {
+  setState: Dispatch<SetStateAction<SignInFlow>>;
 }
+
+export const SignUpCard = ({ setState }: SignUpCardProps) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  return (
+    <Card className="h-full w-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Sign up to continue</CardTitle>
+
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value={formData.email}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, email: e.target.value }))
+            }
+            placeholder="Email"
+            type="email"
+            required
+          />
+
+          <Input
+            disabled={false}
+            value={formData.password}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, password: e.target.value }))
+            }
+            placeholder="Password"
+            type="password"
+            required
+          />
+
+          <Input
+            disabled={false}
+            value={formData.confirmPassword}
+            onChange={(e) =>
+              setFormData((prev) => ({
+                ...prev,
+                confirmPassword: e.target.value,
+              }))
+            }
+            placeholder="Confirm Password"
+            type="password"
+            required
+          />
+
+          <Button size="lg" type="submit" className="w-full">
+            Continue
+          </Button>
+        </form>
+
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button className="relative w-full" size="lg" variant="outline">
+            <FcGoogle className="size-5 absolute top-3 left-2.5" />
+            Continue with Google
+          </Button>
+
+          <Button className="relative w-full" size="lg" variant="outline">
+            <FaGithub className="size-5 absolute top-3 left-2.5" />
+            Continue with GitHub
+          </Button>
+        </div>
+
+        <p className="text-xs text-muted-foreground">
+          Already have an account?
+          <span
+            onClick={() => setState("signIn")}
+            className="text-sky-700 hover:underline cursor-pointer"
+          >
+            Sign in
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
