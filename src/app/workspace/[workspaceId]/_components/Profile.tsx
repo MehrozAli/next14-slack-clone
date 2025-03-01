6cd04a1f069d5bccec2903ae453c3568e7040f2d
Mehroz Ali <dev.mehrozali@gmail.com>
2025-03-01 23:55:56 +0500

    Remove member from workspace + Upgrade/downgrade a member to/from admin role

--- src/app/workspace/[workspaceId]/_components/Profile.tsx
+++ src/app/workspace/[workspaceId]/_components/Profile.tsx
@@ -1,13 +1,33 @@
+import Link from "next/link";
 import React from "react";
-import { AlertTriangle, Loader, MailIcon, XIcon } from "lucide-react";
+import {
+  AlertTriangle,
+  ChevronDown,
+  Loader,
+  MailIcon,
+  XIcon,
+} from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
 import { useGetMember } from "@/features/members/api/useGetMember";
+import { useUpdateMember } from "@/features/members/api/useUpdateMember";
+import { useRemoveMember } from "@/features/members/api/useRemoveMember";
+import { useCurrentMember } from "@/features/members/api/useCurrentMember";
+import { useWorkspaceId } from "@/hooks/useWorkspaceId";
+import { useConfirm } from "@/hooks/useConfirm";
 
 import { Id } from "../../../../../convex/_generated/dataModel";
-import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface ProfileProps {
   memberId: Id<"members">;
@@ -15,11 +35,75 @@ interface ProfileProps {
 }
 
 export const Profile = ({ memberId, onClose }: ProfileProps) => {
+  const router = useRouter();
+  const [LeaveDialog, confirmLeave] = useConfirm(
+    "Are you sure you want to leave this workspace?",
+    "Leave workspace"
+  );
+  const [RemoveDialog, confirmRemove] = useConfirm(
+    "Are you sure you want to remove this member?",
+    "Remove member"
+  );
+  const [UpdateDialog, confirmUpdate] = useConfirm(
+    "Are you sure you want to change this member's roel?",
+    "Update role"
+  );
+  const workspaceId = useWorkspaceId();
+  const { data: currentMember, isLoading: isLoadingCurrentMember } =
+    useCurrentMember({ workspaceId });
   const { data: member, isLoading: isLoadingMember } = useGetMember({
     id: memberId,
   });
+  const { mutate: updateMember, isPending: isUpdatingMember } =
+    useUpdateMember();
+  const { mutate: removeMember, isPending: isRemovingMember } =
+    useRemoveMember();
 
-  if (isLoadingMember) {
+  const onRemoveOrLeave = async (action: "remove" | "leave" = "remove") => {
+    const ok =
+      action === "leave" ? await confirmLeave() : await confirmRemove();
+
+    if (!ok) return;
+
+    removeMember(
+      { id: memberId },
+      {
+        onSuccess: () => {
+          if (action === "leave") {
+            router.replace("/");
+          }
+
+          toast.success(
+            action === "remove" ? "Member removed" : "Left workspace"
+          );
+          onClose();
+        },
+        onError: () =>
+          toast.error(
+            `Failed to ${action === "remove" ? "remove member" : "leave workspace"}`
+          ),
+      }
+    );
+  };
+
+  const onUpdate = async (role: "admin" | "member") => {
+    const ok = await confirmUpdate();
+
+    if (!ok) return;
+
+    updateMember(
+      { id: memberId, role },
+      {
+        onSuccess: () => {
+          toast.success("Member role updated");
+          onClose();
+        },
+        onError: () => toast.error("Failed to update member role"),
+      }
+    );
+  };
+
+  if (isLoadingMember || isLoadingCurrentMember) {
     return (
       <div className="h-full flex flex-col">
         <div className="flex justify-between items-center h-[49px] px-4 border-b">
@@ -55,50 +139,106 @@ export const Profile = ({ memberId, onClose }: ProfileProps) => {
   }
 
   return (
-    <div className="h-full flex flex-col">
-      <div className="flex justify-between items-center h-[49px] px-4 border-b">
-        <p className="text-lg font-bold">Profile</p>
-        <Button onClick={onClose} size="iconSm" variant="ghost">
-          <XIcon className="size-5 stroke-1.5" />
-        </Button>
-      </div>
+    <>
+      <LeaveDialog />
+      <UpdateDialog />
+      <RemoveDialog />
 
-      <div className="flex flex-col items-center justify-center p-4">
-        <Avatar className="max-w-[256px] max-h-[256px] size-full">
-          <AvatarImage src={member.user.image} />
-          <AvatarFallback className="aspect-square text-6xl">
-            {member.user.name?.charAt(0).toUpperCase()}
-          </AvatarFallback>
-        </Avatar>
-      </div>
+      <div className="h-full flex flex-col">
+        <div className="flex justify-between items-center h-[49px] px-4 border-b">
+          <p className="text-lg font-bold">Profile</p>
+          <Button onClick={onClose} size="iconSm" variant="ghost">
+            <XIcon className="size-5 stroke-1.5" />
+          </Button>
+        </div>
 
-      <div className="flex flex-col p-4">
-        <p className="text-xl font-bold">{member.user.name}</p>
-      </div>
-      <Separator />
+        <div className="flex flex-col items-center justify-center p-4">
+          <Avatar className="max-w-[256px] max-h-[256px] size-full">
+            <AvatarImage src={member.user.image} />
+            <AvatarFallback className="aspect-square text-6xl">
+              {member.user.name?.charAt(0).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </div>
 
-      <div className="flex flex-col p-4">
-        <p className="text-sm font-bold mb-4">Contact Information</p>
+        <div className="flex flex-col p-4">
+          <p className="text-xl font-bold">{member.user.name}</p>
 
-        <div className="flex items-center gap-2">
-          <div className="size-9 rounded-md bg-muted flex items-center justify-center">
-            <MailIcon className="size-4" />
-          </div>
+          {currentMember?.role === "admin" &&
+          currentMember?._id !== memberId ? (
+            <div className="flex items-center gap-2 mt-4">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" className="w-full capitalize">
+                    {member.role}
+                    <ChevronDown className="size-4 ml-2" />
+                  </Button>
+                </DropdownMenuTrigger>
 
-          <div className="flex flex-col">
-            <p className="text-[13px] font-semibold text-muted-foreground">
-              Email Address
-            </p>
+                <DropdownMenuContent className="w-full">
+                  <DropdownMenuRadioGroup
+                    value={member.role}
+                    onValueChange={(role) =>
+                      onUpdate(role as "admin" | "member")
+                    }
+                  >
+                    <DropdownMenuRadioItem value="admin">
+                      Admin
+                    </DropdownMenuRadioItem>
 
-            <Link
-              className="text-sm hover:underline text-[#1264a3]"
-              href={`mailto:${member.user.email}`}
-            >
-              {member.user.email}
-            </Link>
+                    <DropdownMenuRadioItem value="member">
+                      Member
+                    </DropdownMenuRadioItem>
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              <Button
+                variant="outline"
+                className="w-full capitalize"
+                onClick={() => onRemoveOrLeave("remove")}
+              >
+                Remove
+              </Button>
+            </div>
+          ) : currentMember?._id === memberId &&
+            currentMember?.role !== "admin" ? (
+            <div className="mt-4">
+              <Button
+                variant="outline"
+                className="w-full"
+                onClick={() => onRemoveOrLeave("leave")}
+              >
+                Leave
+              </Button>
+            </div>
+          ) : null}
+        </div>
+        <Separator />
+
+        <div className="flex flex-col p-4">
+          <p className="text-sm font-bold mb-4">Contact Information</p>
+
+          <div className="flex items-center gap-2">
+            <div className="size-9 rounded-md bg-muted flex items-center justify-center">
+              <MailIcon className="size-4" />
+            </div>
+
+            <div className="flex flex-col">
+              <p className="text-[13px] font-semibold text-muted-foreground">
+                Email Address
+              </p>
+
+              <Link
+                className="text-sm hover:underline text-[#1264a3]"
+                href={`mailto:${member.user.email}`}
+              >
+                {member.user.email}
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
